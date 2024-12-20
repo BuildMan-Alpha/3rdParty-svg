b662e7cdeba742b1173e8bd8066995fe07215857
cian chambliss <cian@alphasoftware.com>
2024-12-20 17:21:49 -0500

    Initial commit

---
+++ 3.2.3/Source/Filter Effects/feComponentTransfer/SvgComponentTransfer.cs
@@ -0,0 +1,16 @@
+﻿namespace Svg.FilterEffects
+{
+    [SvgElement("feComponentTransfer")]
+    public partial class SvgComponentTransfer : SvgFilterPrimitive
+    {
+        public override void Process(ImageBuffer buffer)
+        {
+            // TODO: Implement feComponentTransfer filter Process().
+        }
+
+        public override SvgElement DeepCopy()
+        {
+            return DeepCopy<SvgComponentTransfer>();
+        }
+    }
+}
---
+++ 3.2.3/Source/Filter Effects/feTile/SvgTile.cs
@@ -0,0 +1,16 @@
+﻿namespace Svg.FilterEffects
+{
+    [SvgElement("feTile")]
+    public partial class SvgTile : SvgFilterPrimitive
+    {
+        public override void Process(ImageBuffer buffer)
+        {
+            // TODO: Implement feTile filter Process().
+        }
+
+        public override SvgElement DeepCopy()
+        {
+            return DeepCopy<SvgTile>();
+        }
+    }
+}
